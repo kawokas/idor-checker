bd22a9c077266a7ba2a9027acb76d9e5963e73cb
kawoka <br2jn51@yahoo.co.jp>
2019-02-05 11:23:02 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const sharedConfig = require('./webpack.config.shared')
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin')
+const path = require('path')
+const { env } = require('process')
+
+module.exports = merge(sharedConfig, {
+  mode: 'production',
+  entry: './lib/devtools.js',
+  output: {
+    filename: '[name].js',
+    path:  path.join(__dirname, 'extension', 'dist')
+  },
+  stats: 'normal',
+  plugins: [
+    new webpack.EnvironmentPlugin(JSON.parse(JSON.stringify(env))),
+    new CaseSensitivePathsPlugin()
+  ],
+})
